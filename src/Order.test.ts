aafbddd6687ed93e42143808a9122c7a7970a5ed
José Luiz Rosa Junior <jose.junior@betha.com.br>
2021-12-16 22:13:20 -0300

    first commit

---
+++ src/Order.test.ts
@@ -0,0 +1,6 @@
+import Order from "./Order";
+
+test('Não deve criar um pedido com CPF inválido', () => {
+    const cpf = '123.456.789-10';
+    expect(() => new Order(cpf)).toThrow('CPF inválido');
+});
